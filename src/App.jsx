ee75fe75d1ee63e444b9e757154b1fdcd95d824e
harrifano1997 <richanne.harri@gmail.com>
2025-09-02 03:24:07 +0800

    Removed use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
 import Main from "./components/Main"
 import Services from "./components/Services"
 import About from './components/About'
@@ -7,14 +6,6 @@ import Contact from './components/Contact'
 import Projects from './components/Projects'
 
 function App() {
-  useEffect(() => {
-    const redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-    if (redirect && redirect !== location.href) {
-      history.replaceState(null, null, redirect);
-    }
-  }, []);
-
   return (
     <Router basename={process.env.NODE_ENV === 'production' ? '/Porfolio-Harri' : ''}>
       <Routes>
